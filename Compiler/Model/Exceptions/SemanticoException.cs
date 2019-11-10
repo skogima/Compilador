ce58f52e49a7ba313a308eb59d3732f2ceacdf84
skogima <50327059+skogima@users.noreply.github.com>
2019-11-10 12:46:22 -0300

    Refatoração

---
+++ Compiler/Model/Exceptions/SemanticoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Compiler
+{
+    public class SemanticoException : Exception
+    {
+        public SemanticoException(string message) : base(message)
+        {
+        }
+
+        public SemanticoException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Compiler/Model/Exceptions/SintaticoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Compiler
+{
+    public class SintaticoException : Exception
+    {
+        public SintaticoException(string message) : base(message) { }
+        public SintaticoException(string message, Exception inner) : base(message, inner) { }
+    }
+}
